8c0a9524d340b0f53dc161770959c88c40edee11
_72mahdii <fakharnia.2m@gmail.com>
2019-05-25 16:20:47 +0430

    Update Startup.cs class In this commit update Startup.cs class and add implementation of idsv4

---
+++ Authentication/Config.cs
@@ -0,0 +1,83 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Authentication
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resourceapi", "Resource API")
+                {
+                    Scopes = {
+                        new Scope("api.read"),
+                        new Scope
+                        {
+                            Name = "api1",
+                            DisplayName = "API Access",
+                            Description = "God API",
+                            UserClaims = {"Name", "Role"}
+                        }
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client {
+                    RequireConsent = false,
+                    ClientId = "moamma_spa",
+                    ClientName = "MoammaPey SPA",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = {
+                        "openid",
+                        "profile",
+                        "email",
+                        "api.read"
+                    },
+                    RedirectUris = {"http://moammapey.ir/auth-callback"},
+                    PostLogoutRedirectUris = {"http://moammapey.ir/index"},
+                    AllowedCorsOrigins = {"http://moammapey.ir"},
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600
+                },
+                new Client {
+                    RequireConsent = false,
+                    ClientId = "god_spa",
+                    ClientName = "God SPa",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = {
+                        "openid",
+                        "profile",
+                        "email",
+                        "api.read",
+
+                    },
+                    RedirectUris = {"http://moammapey.ir/god-back"},
+                    PostLogoutRedirectUris = {"http://moammapey.ir/index/"},
+                    AllowedCorsOrigins = {"http://moammapey.ir"},
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600
+                },
+
+
+            };
+        }
+    }
+
+}
--- Authentication/Startup.cs
+++ Authentication/Startup.cs
@@ -1,34 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Authentication.Controllers;
+using Authentication.Models;
+using IdentityServer4.Services;
+using IdentityServer4.AspNetIdentity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Authentication
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AdministrationContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:Author:ConnectionString"]));
+            services.AddIdentity<Author, IdentityRole>()
+                .AddEntityFrameworkStores<AdministrationContext>()
+                .AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(opts =>
+            {
+                opts.Cookie.HttpOnly = true;
+                opts.SlidingExpiration = true;
+                opts.Cookie.Expiration = TimeSpan.FromHours(8);
+                opts.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                opts.LoginPath = "/account/login";
+                opts.LogoutPath = "/account/logout";
+            });
+
+
+            services.AddIdentityServer()
+                .AddSigninCredentialFromConfig(Configuration.GetSection("SigninKeyCredentials"))
+                .AddInMemoryClients(Config.GetClients())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddAspNetIdentity<Author>();
+            services.AddTransient<IProfileService, IdentityClaimsProfileService>();
+            services.AddCors(options =>
+                                options
+                                .AddPolicy("AllowAll", p =>
+                                    p.AllowAnyOrigin()
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader()));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+            app.UseIdentityServer();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Account}/{action=Login}/{id?}"
+                );
             });
+
         }
     }
 }
