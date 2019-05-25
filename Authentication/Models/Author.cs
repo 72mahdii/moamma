9f6c6950e234b586472eac50ab72dde8df349b7c
_72mahdii <fakharnia.2m@gmail.com>
2019-05-25 15:50:55 +0430

    Create Author Model For as IdentityUser

---
+++ Authentication/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Models
+{
+    public class Author : IdentityUser
+    {
+        public string NameInPersian { get; set; }
+        public string Category { get; set; }
+        public Author() { }
+        public Author(string userName) : base(userName) { }
+
+    }
+}
