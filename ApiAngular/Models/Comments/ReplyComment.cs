0d9fc00d848de3a5e899af5eaa4ffb64273ab2d8
_72mahdii <fakharnia.2m@gmail.com>
2019-05-25 17:04:42 +0430

    Prepare Database Model In this  commit prepare database model for Article, DeletedArticle, Comment, ReplyComment

---
+++ ApiAngular/Models/Comments/ReplyComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAngular.Models.Comments
+{
+    public class ReplyComment
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+        public bool Approved { get; set; }
+        public bool Vip { get; set; }
+        public long CommentId { get; set; }
+    }
+}
---
+++ ApiAngular/Models/Comments/SendComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAngular.Models.Comments
+{
+    public class SendComment
+    {
+        public string Text { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long ArticleId { get; set; }
+    }
+}
