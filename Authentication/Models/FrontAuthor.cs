be73a5d477ebd50d457a66c284f6e1bd238ec151
_72mahdii <fakharnia.2m@gmail.com>
2019-05-25 15:56:12 +0430

    Add FrontAuthor Class This author class is for front-end display of authors.

---
+++ Authentication/Models/FrontAuthor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Models
+{
+    public class FrontAuthor
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string ImgAddress { get; set; }
+        public string Category { get; set; }
+    }
+}
