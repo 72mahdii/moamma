4c00faddea90923d47ed6468d4b3cb47a4f5ede4
_72mahdii <fakharnia.2m@gmail.com>
2019-05-25 16:23:03 +0430

    Add SigninCredentialExtension class Add this class for implement certificate from file

---
+++ Authentication/Controllers/SigninCredentialExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace Authentication.Controllers
+{
+    public static class SigninCredentialExtension
+    {
+        private const string KeyFilePath = "KeyFilePath";
+        private const string KeyFilePassword = "KeyFilePassword";
+
+        public static IIdentityServerBuilder AddSigninCredentialFromConfig(
+            this IIdentityServerBuilder builder,
+            IConfigurationSection options
+            )
+        {
+            var keyFilePath = options.GetValue<string>(KeyFilePath);
+            var keyFilePassword = options.GetValue<string>(KeyFilePassword);
+
+            if (File.Exists(keyFilePath))
+            {
+                builder.AddSigningCredential(new X509Certificate2(
+                    keyFilePath, keyFilePassword,
+                    X509KeyStorageFlags.MachineKeySet
+                    ));
+            }
+            return builder;
+        }
+    }
+}
